f93e2fe906a70e14b73905be1a52acaa5bc0c469
PavelB1349 <daz1349@gmail.com>
2024-07-11 14:58:32 +0500

    First commit

---
+++ GitTestApp/GitTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            var b = 6;
+            var c = 12;
+
+            Console.WriteLine($"Result = {a + b + c}");
+            Console.ReadKey();
+        }
+    }
+}
